11dbc62e6d437327e1a4c3594e15d8dc94515acb
Nikolay Novik <nickolainovik@gmail.com>
2022-01-02 01:24:43 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+# UDDSketch
+
+A rust implementation of a UDDSketch algorithm.
+
+# Lincese
+ Licensed under the Apache License, Version 2.0
---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+//! A rust implementation of a  UDDSketch algorithm
+
+pub struct UDDSketch {}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
